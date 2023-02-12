f406ca8dc8fd5fa3a9a1c053362437186a2ff4f1
DKS <dksgroup@yandex.ru>
2023-02-13 02:04:32 +0300

    add typescript

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require('express')
 
-const app = express();
-const PORT = 3000;
+const app = express()
+const PORT = 3000
 
-app.use(express.static(__dirname + '/../dist/'));
+app.use(express.static(__dirname + '/../dist/'))
 app.listen(PORT, function () {
-  console.log(`http://localhost:${PORT}`);
-});
+  console.log(`http://localhost:${PORT}`)
+})
